64a938c8a55e816a65d41b9ed10c44364de029f8
xjx00 <1504635269@qq.com>
2020-08-14 18:43:33 +0800

    [skip ci]

---
+++ build_win.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt update
+sudo apt install gcc-mingw-w64-x86-64
+
+CGO_ENABLED=1 
+GOOS=windows 
+GOARCH=amd64 
+CC=x86_64-w64-mingw32-gcc
+go build -o build/temp/casgo_win64.exe app.go
